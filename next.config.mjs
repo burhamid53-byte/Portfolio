eadefa9e8bea3158cf722b1c66c5cbd16fa65a30
Burhanuddin Hamid <burhamid53@gmail.com>
2025-12-24 15:22:07 +0530

    chore: update next.config.mjs to remove unused plugins and maintain page extensions

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
-import rehypePrism from '@mapbox/rehype-prism'
 import nextMDX from '@next/mdx'
-import remarkGfm from 'remark-gfm'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+  experimental: {
+    mdxRs: true, // ✅ REQUIRED for Turbopack
+  },
   outputFileTracingIncludes: {
     '/articles/*': ['./src/app/articles/**/*.mdx'],
   },
@@ -12,10 +13,6 @@ const nextConfig = {
 
 const withMDX = nextMDX({
   extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [remarkGfm],
-    rehypePlugins: [rehypePrism],
-  },
 })
 
 export default withMDX(nextConfig)
